8404cd18a0e9f36995fca179fa8376ee6531b9f4
shanifox16 <shanifox16@gmail.com>
2019-10-22 18:39:27 -0400

    Configured CarrierWave in Rails for image uploading and Amazon AWS

---
+++ spec/features/profile_photo_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'profile photo' do
+  scenario 'user uploads a profile photo' do
+    visit new_user_registration_path
+
+    select('student', from: 'Role')
+    fill_in 'Email', with: 'john@example.com'
+    fill_in 'Username', with: 'john123'
+    fill_in 'First Name', with: 'John'
+    fill_in 'Last Name', with: 'Doe'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    attach_file :user_profile_photo, "#{Rails.root}/app/assets/images/shani_profile_photo.jpg"
+
+    click_button 'Sign up'
+
+    expect(page).to have_content("Welcome! You have signed up successfully")
+  end
+end
