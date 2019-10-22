e09f95dba86cf7304e2c22baa1e3b66f97d12977
shanifox16 <shanifox16@gmail.com>
2019-10-22 13:01:54 -0400

    Updated devise tests

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -3,6 +3,10 @@ require 'factory_bot'
 FactoryBot.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
+    sequence(:username) {|n| "user#{n}" }
+    sequence(:fname) {|n| "firstuser#{n}" }
+    sequence(:lname) {|n| "lastuser#{n}" }
+    role { 'student' }
     password { 'password' }
     password_confirmation { 'password' }
   end
