b7935f68c833eaa8e9763290ef0f77487d248be7
shanifox16 <shanifox16@gmail.com>
2019-10-31 17:12:00 -0400

    google api fixes

--- app/javascript/react/components/dashboard/InstructorDashboardContainer.js
+++ app/javascript/react/components/dashboard/InstructorDashboardContainer.js
@@ -14,18 +14,22 @@ const InstructorDashboardContainer = props => {
         />
       </div>
       <div className="dashboard-squares grid-x grid-margin-x cell small-12 medium-6 large-8">
-        <div className="event-container top-left-square cell small-12 large-6 text-center">
-          <EventContainer
-            events={props.events}
-          />
+        <div className="cell top-row-squares grid-x">
+          <div className="event-container top-left-square cell small-12 large-6 text-center">
+            <EventContainer
+              events={props.events}
+              />
+          </div>
+          <div className="top-right-square cell small-12 large-6">
+          </div>
         </div>
-        <div className="top-right-square cell small-12 large-6">
-        </div>
-        <div className="bottom-left-square cell small-12 large-6">
-        </div>
-        <div className="buttons-container bottom-right-square cell small-12 large-6">
-          <ButtonsContainer
-          />
+        <div className="cell bottom-row-squares grid-x">
+          <div className="bottom-left-square cell small-12 large-6">
+          </div>
+          <div className="buttons-container bottom-right-square cell small-12 large-6">
+            <ButtonsContainer
+              />
+          </div>
         </div>
       </div>
     </div>
