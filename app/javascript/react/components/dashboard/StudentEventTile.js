af76c987863962d08837449a205a501a538d4755
shanifox16 <shanifox16@gmail.com>
2019-11-06 11:28:17 -0500

    Subscriber state fixes, README update, student calendar fixes

--- app/javascript/react/components/dashboard/StudentEventTile.js
+++ app/javascript/react/components/dashboard/StudentEventTile.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const StudentEventTile = props => {
   return(
-    <div>
-      <p>{props.eventDate}: {props.name} at {props.eventTime}</p>
+    <div className="event-tile grid-x grid-margin-x">
+      <div id="event-time" className="event-time cell small-3">
+        {props.eventDate}
+      </div>
+      <div id="event-name" className="event-name cell small-9 text-left">
+        {props.name}
+      </div>
     </div>
   )
 }
