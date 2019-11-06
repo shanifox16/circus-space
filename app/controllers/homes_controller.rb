356ce98cffdcda9aa0e7c9e413fb2d0b215175ab
shanifox16 <shanifox16@gmail.com>
2019-11-06 12:22:45 -0500

    Restricted student authorization and removed button outlines

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,17 @@
 class HomesController < ApplicationController
   before_action :authenticate_user!
+  before_action :authorize_user, only: [:create]
 
   def index
+  end
+
+  def create
+  end
+
+  protected
+  def authorize_user
+    if !user_signed_in? || current_user.role != "instructor"
+      raise ActionController::RoutingError.new("As a student, you do not have access to this page")
+    end
   end
 end
