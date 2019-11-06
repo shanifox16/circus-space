b57f08c7dcbeac7691b16fa5077ff21bd8390733
shanifox16 <shanifox16@gmail.com>
2019-11-05 20:32:21 -0500

    Fixed images and updated navigation

--- app/javascript/react/components/dashboard/VideoButtonsContainer.js
+++ app/javascript/react/components/dashboard/VideoButtonsContainer.js
@@ -7,13 +7,13 @@ const VideoButtonsContainer = props => {
   return(
     <div className="video-buttons-container">
       <div className="grid-x grid-margin-x">
-        <Link className="button button-top cell small-6 small-offset-3" to="/personal_videos/new">Post Video</Link><br />
+        <Link className="button button-top cell small-6 small-offset-3" to="/personal_videos">Public Videos</Link>
       </div>
       <div className="grid-x grid-margin-x">
         <Link className="button cell small-6 small-offset-3" to={`/users/${props.currentUser.id}/personal_videos`}>My Videos</Link>
       </div>
       <div className="grid-x grid-margin-x">
-        <Link className="button cell small-6 small-offset-3" to="/personal_videos">Public Videos</Link>
+        <Link className="button cell small-6 small-offset-3" to="/personal_videos/new">Post Video</Link><br />
       </div>
     </div>
   )
