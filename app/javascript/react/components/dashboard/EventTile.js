32e0b1dc6001fd23229b490e96676d18548891bf
shanifox16 <shanifox16@gmail.com>
2019-11-07 12:04:08 -0500

    Media queries for screen sizes

--- app/javascript/react/components/dashboard/EventTile.js
+++ app/javascript/react/components/dashboard/EventTile.js
@@ -3,10 +3,10 @@ import React from 'react'
 const EventTile = props => {
   return(
     <div className="event-tile grid-x grid-margin-x">
-      <div id="event-time" className="event-time cell small-3">
+      <div id="event-time" className="event-time cell small-3 xlarge-3">
         {props.eventTime}
       </div>
-      <div id="event-name" className="event-name cell small-9 text-left">
+      <div id="event-name" className="event-name cell small-9 xlarge-3 text-left">
         {props.name}
       </div>
     </div>
